adba68cf0a311550d74bc6ae75340d54f8cd4591
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2024-09-19 17:02:28 +0700

    Add wait_downloaded_file fn, dl_config comman

--- routers/common.py
+++ routers/common.py
@@ -1,22 +1,53 @@
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
 
-def setup_webdriver(enable_gui: bool) -> WebDriver:
+download_dir = os.path.join(os.getcwd(), 'downloads')
+
+def setup_webdriver(allowed_insecure_ip: str, enable_gui: bool) -> WebDriver:
     # Automatically install the latest chromedriver
     chromedriver_autoinstaller.install()
     # Set up Chrome options
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {
+        "download.default_directory": download_dir,       # Set the download directory
+        "download.prompt_for_download": False,            # Disable download prompts
+        "directory_upgrade": True,                        # Automatically overwrite the download directory
+        "safebrowsing.enabled": True                      # Enable safe browsing
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    # chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--allow-running-insecure-content")
+    chrome_options.add_argument(f"--unsafely-treat-insecure-origin-as-secure=http://{allowed_insecure_ip},https://{allowed_insecure_ip}")
     if not enable_gui:
         chrome_options.add_argument("--headless")  # Run in headless mode (no GUI)
     # Create a new instance of the Chrome driver
     service = Service()
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
+
+def wait_for_download_to_complete(file_name: str, timeout: int=30):
+    os.makedirs(download_dir, exist_ok=True)
+    end_time = time.time() + timeout
+    while time.time() < end_time:
+        # Check if the target file exists and no temporary file is present
+        file_path = os.path.join(download_dir, file_name)
+        temp_file_path = file_path + '.crdownload'
+        if os.path.exists(file_path) and not os.path.exists(temp_file_path):
+            return file_path
+        time.sleep(1)
+    return None
+
+def rename_downloaded_file(old_file_name: str, new_file_name: str) -> str:
+    new_file = os.path.join(download_dir, new_file_name)
+    os.rename(old_file_name, new_file)
+    return new_file
 
 def detect_router(driver: WebDriver, router_ip: str) -> str:
     driver.get(f"http://{router_ip}/")
@@ -36,3 +67,8 @@ def proceed_command(router: any, command: str) -> None:
         router.get_homepage()
         router.do_login()
         print(router.get_wifi_info())
+    elif command == 'dl_config':
+        router.get_homepage()
+        router.do_login()
+        router.download_user_config()
+
